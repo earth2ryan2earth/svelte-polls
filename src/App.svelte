b65a628fdd3ae5a53d51196226904b0927be38cf
Ryan Lee <earth2ryan2earth@gmail.com>
2022-08-15 19:40:35 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
+	import PollList from './components/PollList.svelte';
+	import Tabs from './shared/Tabs.svelte';
+
+	//§ tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	};
+
+	//§ polls
+	const handleAddNewPoll = (e) => {
+		activeItem = 'Current Polls';
+	};
+</script>
+
+<Header />
+<main>
+	<Tabs {activeItem} {items} on:tabChange={tabChange} />
+	{#if activeItem === 'Current Polls'}
+		<PollList />
+	{:else if activeItem === 'Add New Poll'}
+		<CreatePollForm on:addNewPoll={handleAddNewPoll} />
+	{/if}
+</main>
+<Footer />
+
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
